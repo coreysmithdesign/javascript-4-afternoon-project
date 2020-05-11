c76cf43c461177b9eb5dc698e49de038adefcd7a
Corey Smith <corey@coreysmith.design>
2020-05-11 11:56:08 -0700

    Solution

--- constructors.js
+++ constructors.js
@@ -102,15 +102,12 @@ function User(name, age, email, savedPosts) {
   this.name = name;
   this.age = age;
   this.email = email;
-  this.savedPosts = [
-    (this.id = id),
-    (this.title = title),
-    (this.rating = rating),
-  ];
+  this.savedPosts = savedPosts;
 }
 
 User.prototype.addSavedPost = function (id, title, rating) {
-  return { id: id, title: title, rating: rating };
+  newPost = { id: id, title: title, rating: rating };
+  return this.savedPosts.push(newPost);
 };
 
 ////////// PROBLEM 6 //////////
@@ -118,12 +115,21 @@ User.prototype.addSavedPost = function (id, title, rating) {
 // You will be using the constructor function you just created in problem 5.
 // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
 
-// Code here
-User.prototype.removeSavedPost = function (id) {};
+User.prototype.removeSavedPost = function (id) {
+  let found = this.savedPosts.indexOf(this.savedPosts[id]);
+  this.savedPosts.splice(found, 1);
+};
 
 ////////// PROBLEM 7 //////////
 
 // You will continue to use the constructor function you created in problem 5.
 // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
 
-// Code here
+User.prototype.changePostRating = function (id, rating) {
+  this.savedPosts = this.savedPosts.map((event) => {
+    if (event.id === id) {
+      event.rating = rating;
+    }
+    return event;
+  });
+};
